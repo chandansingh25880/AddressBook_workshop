2be3e192e3b65c260d2a437d969fc5b43e3b6a28
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-05-10 03:56:07 +0530

    Added: To read or write file using CSV

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,5 +1,6 @@
 package com.addressbook;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,6 +12,95 @@ import java.util.stream.IntStream;
 /* @Description - To create a contacts in address book with first name, last name, address, city, state,
  * zip,mobile number.*/
 public class AddressBook {
+    public static void writeCsv(String filePath) {
+        List<Contact> contact = new ArrayList<Contact>();
+        Contact contactList = new Contact(filePath, filePath, filePath, filePath, filePath, filePath, filePath,
+                filePath, filePath);
+        contactList.setFirstName("chandan");
+        contactList.setLastName("singh");
+        contactList.setAddress("milling tonia");
+        contactList.setCity("lko");
+        contactList.setState("up");
+        contactList.setZip(226001);
+        contactList.setPhoneNumber(8052636931L);
+        contactList.setEmailId("chandan@gmail.com");
+        contacts.add(contact);
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(filePath);
+            fileWriter.append("firstName,lastName,address,city,state,zip,mobileNumber,EmailId");
+            for (Contact contact : contact) {
+                fileWriter.append(String.valueOf(contact.getFirstName()));
+                fileWriter.append(String.valueOf(contact.getLastName()));
+                fileWriter.append(String.valueOf(contact.getAddress()));
+                fileWriter.append(String.valueOf(contact.getCity()));
+                fileWriter.append(String.valueOf(contact.getState()));
+                fileWriter.append(String.valueOf(contact.getZip()));
+                fileWriter.append(String.valueOf(contact.getPhoneNumber()));
+                fileWriter.append(String.valueOf(contact.getEmailId()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void readCsv(String filePath) {
+        BufferedReader reader = null;
+        try {
+            List<Contacts> empContacts = new ArrayList<Contacts>();
+            String line = "";
+            reader = new BufferedReader(new FileReader(filePath));
+            reader.readLine();
+            while (( reader.readLine() != null)) {
+                String[] field = line.split(",");
+                if (!(field.length() <= 0)) {
+                    Contacts contacts = new Contacts(field, field, field, field, field, field, field, field,
+                            field);
+                    contacts.setFirstName("Prahlad");
+                    contacts.setLastName("Prajapati");
+                    contacts.setAddress("Azamgarh");
+                    contacts.setCity("Azamgarh");
+                    contacts.setState("up");
+                    contacts.setZip(223223);
+                    contacts.setPhoneNumber(8052636931l);
+                    contacts.setEmailId("amarprajapati99@gmail.com");
+                    contacts.add(contacts);
+                }
+            }
+            for (Contacts ad : empContacts) {
+                System.out.printf(
+                        "First Name = %s,Last Name = s, Address = %s, City = %s, State = %s,Zip = %s,Phone Number = %s,Email = %s",
+                        ad.getFirstName(), ad.getLastName(), ad.getAddress(), ad.getCity(), ad.getState(), ad.getZip(),
+                        ad.getMobileNumber(), ad.getEmailId());
+            }
+        } catch (Exception e) {
+            e.fillInStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\Desktop\\CsvReader\\AddressBookIO";
+        System.out.println("Starting File writing :" + filePath);
+        writeCsv(filePath);
+        System.out.println("starting read file");
+        readCsv(filePath);
+
+    }
+}
+
     public static List<Contact> contactList = new ArrayList<>();
     public static HashMap<String, List<Contact>> addressBookMap = new HashMap<>();
     private HashMap<Object, Object> contact;
